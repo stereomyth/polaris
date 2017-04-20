57a1dbf80d62df66393e120ff8ce652aef207fb4
James King <james@wearecrystal.uk>
2017-04-20 15:24:36 +0100

    more bug fixes

--- node/routes/index.js
+++ node/routes/index.js
@@ -23,7 +23,7 @@ let berlin = { lat: 52.5200070, long: 13.4049540 };
 router.get('/', function(req, res, next) {
 
   let map = 'https://www.google.com/maps/embed/v1/directions?' +
-    'origin=' + req.params.lat + '%20' + req.params.long + 
+    'origin=' + home.lat + '%20' + home.long + 
     '&destination=' + berlin.lat + '%20' + berlin.long + 
     '&key=' + $g.mapKey;
 
