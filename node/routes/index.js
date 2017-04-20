3bbdbe15f4c7a6a9d79cd8d6cd22788c9f4c196e
James King <james@wearecrystal.uk>
2017-04-20 10:52:39 +0100

    Lat long params

--- node/routes/index.js
+++ node/routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:lat/:long', function(req, res, next) {
+  let berlin = { lat: 52.5200070, long: 13.4049540};
+
+  res.render('index', { title: 'lol', loc: req.params, berlin: berlin });
 });
 
 module.exports = router;
