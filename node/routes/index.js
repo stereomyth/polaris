59429557849bde1e1242c4054831539cab662878
James King <james@wearecrystal.uk>
2017-04-20 11:55:56 +0100

    Google map display

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var $g = require('../env.js');
 
 /* GET home page. */
 router.get('/:lat/:long', function(req, res, next) {
+
   let berlin = { lat: 52.5200070, long: 13.4049540};
 
-  res.render('index', { title: 'lol', loc: req.params, berlin: berlin });
+  let map = 'https://www.google.com/maps/embed/v1/directions?' +
+    'origin=' + req.params.lat + '%20' + req.params.long + 
+    '&destination=' + berlin.lat + '%20' + berlin.long + 
+    '&key=' + $g.mapKey;
+
+  res.render('index', { loc: req.params, berlin: berlin, src: map, bearing: '' });
 });
 
 module.exports = router;
