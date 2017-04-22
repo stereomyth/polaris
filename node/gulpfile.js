3d30f20751c9e9676bb53e4ae44c919700338b5b
James King <j@lostwith.me>
2017-04-22 12:33:55 +0100

    babel for front end js

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -10,6 +10,12 @@ gulp.task('styles', function () {
     .pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
     .pipe(gulp.dest('./public/styles'));
 });
+
+gulp.task('scripts', function () {
+  return gulp.src('./dev/scripts/**/*.js')
+  .pipe($.babel({ presets: ['es2015'] }))
+  .pipe(gulp.dest('./public/scripts'));
+});
  
 gulp.task('serve', ['styles'], function () {
   gulp.watch('./dev/styles/**/*.scss', ['styles']);
