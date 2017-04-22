cdc3ab83111bff2b8ca2849bd948dfb108aba8c8
James King <j@lostwith.me>
2017-04-22 11:45:38 +0100

    add autoprefixer

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -4,12 +4,13 @@ var gulp = require('gulp');
 var loadPlugins = require('gulp-load-plugins');
 var $ = loadPlugins();
 
-gulp.task('sass', function () {
+gulp.task('styles', function () {
   return gulp.src('./dev/styles/**/*.scss')
     .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
     .pipe(gulp.dest('./public/styles'));
 });
  
 gulp.task('serve', function () {
-  gulp.watch('./dev/styles/**/*.scss', ['sass']);
+  gulp.watch('./dev/styles/**/*.scss', ['styles']);
 });
