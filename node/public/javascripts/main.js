cf1d6096a9b99b317207087fb8c19d1c6277eb79
James King <j@lostwith.me>
2017-04-22 10:29:48 +0100

    get current cords with location api

--- node/public/javascripts/main.js
+++ node/public/javascripts/main.js
@@ -1,7 +1,4 @@
-document.getElementById('set-btn').addEventListener('click', event => {
-  let lat = document.getElementById('lat').value;
-  let long = document.getElementById('long').value;
-
+let setCoords = (lat, long) => {
   if (lat && long) {
     fetch('/set', {
       method: 'post',
@@ -14,6 +11,28 @@ document.getElementById('set-btn').addEventListener('click', event => {
       console.log('error', err);
     });
   }
+};
 
+document.getElementById('set-btn').addEventListener('click', event => {
+  let lat = document.getElementById('lat').value;
+  let long = document.getElementById('long').value;
+
+  setCoords(lat, long);
 }, false);
+
+if ("geolocation" in navigator) {
+  document.getElementById('geo-btn').className += ' visible';
+}
+
+document.getElementById('geo-btn').addEventListener('click', event => {
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let lat = document.getElementById('lat').value = position.coords.latitude.toFixed(3);
+    let long = document.getElementById('long').value = position.coords.longitude.toFixed(3);
+
+    setCoords(lat, long);
+  });
+  
+}, false);
+
 
