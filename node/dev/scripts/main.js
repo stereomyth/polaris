4162a5710fdc8f3e76da0b9dafe4b3dd405a3c76
James King <james@wearecrystal.uk>
2017-05-05 17:56:19 +0100

    Convert to jquery

--- node/dev/scripts/main.js
+++ node/dev/scripts/main.js
@@ -1,38 +1,40 @@
-/* globals document, fetch, navigator, alert, console */
-'use strict';
+/* globals $, document, fetch, navigator, alert, console */
+(function () {
+  'use strict';
 
-let setCoords = (lat, long) => {
-  if (lat && long) {
-    fetch('/set', {
-      method: 'post',
-      body: JSON.stringify({ lat: lat, long: long }),
-      headers: { 'Content-Type': 'application/json' },
-    }).then(res => {
-      console.log('success');
-      document.getElementById('update-success').className += ' visible';
-    }).catch(err => {
-      console.log('error', err);
-    });
-  }
-};
+  let setCoords = (lat, long) => {
+    if (lat && long) {
+      fetch('/set', {
+        method: 'post',
+        body: JSON.stringify({ lat: lat, long: long }),
+        headers: { 'Content-Type': 'application/json' },
+      }).then(res => {
+        console.log('success');
+        $('.update-success').addClass('visible');
+      }).catch(err => {
+        console.log('error', err);
+      });
+    }
+  };
 
-document.getElementById('set-btn').addEventListener('click', event => {
-  let lat = document.getElementById('lat').value;
-  let long = document.getElementById('long').value;
-
-  setCoords(lat, long);
-}, false);
-
-if ("geolocation" in navigator) {
-  document.getElementById('geo-btn').className += ' visible';
-}
-
-document.getElementById('geo-btn').addEventListener('click', event => {
-  navigator.geolocation.getCurrentPosition( position => {
-    let lat = document.getElementById('lat').value = position.coords.latitude.toFixed(3);
-    let long = document.getElementById('long').value = position.coords.longitude.toFixed(3);
+  $('.set-btn').on('click', function () {
+    let lat = $('#lat').val();
+    let long = $('#long').val();
 
     setCoords(lat, long);
-  },
-  error => { alert(error.message); });
-}, false);
+  });
+
+  if ("geolocation" in navigator) {
+    $('.geo-btn').show();
+  }
+
+  $('.geo-btn').on('click', function () {
+    navigator.geolocation.getCurrentPosition( position => {
+      let lat = $('#lat').val(position.coords.latitude.toFixed(3));
+      let long = $('#long').val(position.coords.longitude.toFixed(3));
+
+      setCoords(lat, long);
+    },
+    error => { alert(error.message); });
+  });
+}());
