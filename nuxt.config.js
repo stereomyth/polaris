fbc3a46a0e96a33e2250bba07662fb4659344f3d
James King <j@lostwith.me>
2019-04-13 11:52:03 +0100

    theme colour and typekit

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'theme-color', content: '#222' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'stylesheet', href: 'https://use.typekit.net/cmb4wle.css' },
+    ],
   },
 
   loading: { color: '#fff' },
