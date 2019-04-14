b0da4d20a32a5a1bb68eb4f4321fcf6eebc47d72
James King <j@lostwith.me>
2019-04-14 09:53:08 +0100

    convert express routes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # polaris
 
 > stella illa quae polaris dicitur
+
+## Old Routes
+
+GET / to return list of tweets with bearings
+GET /bearing to return single bearing
+POST coords to /set = save coords to json file
--- functions/seek.js
+++ functions/seek.js
@@ -1,6 +1,23 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: 'Hello, World'
-  })
-}
+    body: 'Hello, World',
+  });
+};
+
+// toRad = deg => deg * Math.PI / 180;
+// toDeg = rad => rad * 180 / Math.PI;
+
+// let bearing = (start, end) => {
+//   var φ1 = toRad(start.lat), φ2 = toRad(end.lat);
+//   var Δλ = toRad(end.long - start.long);
+
+//   var y = Math.sin(Δλ) * Math.cos(φ2);
+//   var x = Math.cos(φ1) * Math.sin(φ2) - Math.sin(φ1) * Math.cos(φ2) * Math.cos(Δλ);
+//   var θ = Math.atan2(y, x);
+
+//   return (toDeg(θ) + 360) % 360;
+// };
+
+// let home   = { lat: 52.4720187, long: -1.9104001 };
+// let berlin = { lat: 52.5200070, long: 13.4049540 };
