b68daab1b1c175d3ee41f7b3ba29276ab43b8e6c
James King <j@lostwith.me>
2019-05-29 22:41:03 +0100

    destination component

---
+++ components/Destination.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  props: ["loc"],
+
+  methods: {},
+
+  data: () => ({}),
+
+  computed: {}
+};
+</script>
+
+<template>
+  <div>
+    <h2>Destination</h2>
+
+    <template v-if="dest">
+      <div class="tweet">
+        <div class="meta">
+          <strong>{{dest.name}}</strong>
+          <small>@{{dest.user}}</small>
+        </div>
+      </div>
+      <p>{{dest.text}}</p>
+
+      <hr>
+
+      <div class="row">
+        <div class="col">
+          <label for>Latitude</label>
+          <div class="faux-input">{{dest.lat}}</div>
+        </div>
+        <div class="col">
+          <label for>Longitude</label>
+          <div class="faux-input">{{dest.long}}</div>
+        </div>
+      </div>
+
+      <div class="compass-flex">
+        <div class="bearing-contain">
+          <label>Bearing</label>
+          <div class="faux-input">{{dest.bearing || 'unkown'}}°</div>
+        </div>
+        <div class="compass-contain">
+          <div class="compass">
+            <div class="needle" :style="`transform: rotate(${dest.bearing}deg)`"></div>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <p>No destination can be found</p>
+    </template>
+
+    <!-- <hr> -->
+  </div>
+</template>
+
+<style lang="scss">
+</style>
